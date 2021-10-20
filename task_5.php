182c8d25e36c275dd7326a36b10ce4f74e9cafd9
Saberzhan19 <Saberzhax@gmail.com>
2021-10-20 06:01:15 +0600

    add comments with explaination

--- task_5.php
+++ task_5.php
@@ -91,6 +91,7 @@
                                 <div class="d-flex flex-wrap demo demo-h-spacing mt-3 mb-3 ">
                                <?php foreach ($man as $key):?>
                             <div class="rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0  <? echo $key ['status'] === false ? 'absent' : '';?>">
+                            <!-- echo $key ['status']  если равняеться == в false то выводим ? 'absent' иначе : '' в кавычках пустота ; -->
                                 <img src="<? echo $key["src"];?>" alt="<? echo $key["alt"];?>" class="img-thumbnail img-responsive rounded-circle" style="width:5rem; height: 5rem;">
                                 <div class="ml-2 mr-3 ">
                                     <h5 class="m-0">
